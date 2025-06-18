55acea13c231c9e532c9bf840b96c5a185391e68
shrinivasa1 <135809488+shrinivasa1@users.noreply.github.com>
2025-06-18 16:14:45 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Menu from "./pages/Menu";
 import Pagenotfound from "./pages/Pagenotfound";
-
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/menu" element={<Menu />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register/>} />
           <Route path="*" element={<Pagenotfound />} />
         </Routes>
       </BrowserRouter>
